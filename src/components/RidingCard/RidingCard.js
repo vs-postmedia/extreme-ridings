633f88e2c6982ee21a490e3c2aa32964a70e667f
Nathan Griffiths <ngriffiths@postmedia.com>
2019-10-07 15:08:06 -0700

    added carousel for riding cards

--- src/components/RidingCard/RidingCard.js
+++ src/components/RidingCard/RidingCard.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import './RidingCard.css';
 
+import locator from './images/test-04.png';
+
 const RidingCard = (props) => {
 	return (
 		<li className="riding-card">
 			<header>
 				<h3>Kamloops—Thompson—Cariboo</h3>
-			</header>
-			<div className="map">
 				<p className="subhead">B.C.</p>
-			</div>
+			</header>
 			
 			<div className="copy">
-				<p className="percent">35%</p>
-				<p className="variable-description">spend over 30% of their after-tax income on housing.</p>
-			</div>
-			<div className="eln-results">
+				<div className="gfx">
+					<p className="percent">35%</p>
+					<img className="map" src={locator} alt="locator map"/>
+				</div>
+				<div>
+					<p className="variable-description">spend over 30% of their after-tax income on housing.</p>
+				
 				<p className="subhead">Vote history</p>
-				<div className="swatch liberal"></div>
-				<p className="eln-year">’15</p>
-				<div className="swatch conservative"></div>
-				<p className="eln-year">’11</p>
-				<div className="swatch conservative"></div>
-				<p className="eln-year">’08</p>
-				<div className="swatch ndp"></div>
-				<p className="eln-year">’06</p>
+				<div className="eln-results">	
+					<div className="swatch liberal"></div>
+					<p className="eln-year">’15</p>
+					<div className="swatch conservative"></div>
+					<p className="eln-year">’11</p>
+					<div className="swatch conservative"></div>
+					<p className="eln-year">’08</p>
+					<div className="swatch ndp"></div>
+					<p className="eln-year">’06</p>
+				</div>
+				</div>
 			</div>
 		</li>
 	)
@@ -32,4 +38,4 @@ const RidingCard = (props) => {
 
 export default RidingCard;
 
-// <img src="https://via.placeholder.com/125x75" />
+// <img className="map" src="https://via.placeholder.com/150x150" />
--- src/components/RidingResults/RidingResults.js
+++ src/components/RidingResults/RidingResults.js
@@ -1,30 +1,45 @@
 import React, { Fragment } from 'react';
+import Slider from 'react-slick';
 import RidingCard from '../RidingCard/RidingCard';
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import './RidingResults.css';
 
 const cards = [1,2,3];
 const RidingResults = (props) => {
+	const settings = {
+		centerMode: true,
+		centerPadding: '10px',
+		dots: false,
+		infinite: false,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1
+    };
+
 	return (
 		<Fragment>
 			<p className="text">Lorem ipsum text about party platform</p>
 
-			<ul className="ridings-container">
-				<p className="sub-head">Ridings most affected</p>
-
+			<p className="sub-head">Ridings most affected</p>
+			<Slider className="ridings-container" {...settings}>
 				{ 
 					cards.map(card => {
 						return <RidingCard key={`affected-${card}`}></RidingCard>
 					})
 				}
-			</ul>
-			<ul className="ridings-container">
-				<p className="sub-head">Ridings least affected</p>
+			</Slider>
+
+			<p className="sub-head">Ridings least affected</p>
+			<Slider className="ridings-container" {...settings}>
+				
 				{ 
 					cards.map(card => {
 						return <RidingCard key={`unaffected-${card}`}></RidingCard>
 					})
 				}
-			</ul>
+			</Slider>
 		</Fragment>
 	)
 }
