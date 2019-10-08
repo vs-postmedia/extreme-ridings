3697a3048f45fca0ed297729432ea17418ce0d33
mr_ <njgriffiths@gmail.com>
2019-10-08 15:54:14 -0700

    Updates

---
+++ precache-manifest.ac3f5d6ff8f29a40afe82600ed57c02a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5332c1f636dcba952d17bbca93349939",
+    "url": "/extreme-ridings/index.html"
+  },
+  {
+    "revision": "20edff701ec740a9ed70",
+    "url": "/extreme-ridings/static/css/2.89e5efac.chunk.css"
+  },
+  {
+    "revision": "8eb33ece14db644054f1",
+    "url": "/extreme-ridings/static/css/main.28e8a536.chunk.css"
+  },
+  {
+    "revision": "20edff701ec740a9ed70",
+    "url": "/extreme-ridings/static/js/2.7f1436e2.chunk.js"
+  },
+  {
+    "revision": "8eb33ece14db644054f1",
+    "url": "/extreme-ridings/static/js/main.c233fb4b.chunk.js"
+  },
+  {
+    "revision": "7ed47f8f114e27ee5bbc",
+    "url": "/extreme-ridings/static/js/runtime~main.32e6952c.js"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/extreme-ridings/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/extreme-ridings/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/extreme-ridings/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/extreme-ridings/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/extreme-ridings/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/extreme-ridings/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  },
+  {
+    "revision": "3f86fefa2240e3220a2ea154f712503d",
+    "url": "/extreme-ridings/static/media/map.3f86fefa.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/extreme-ridings/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/extreme-ridings/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/extreme-ridings/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/extreme-ridings/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c721d5a947e5a3b595a123a585c3f295",
+    "url": "/extreme-ridings/static/media/test-01.c721d5a9.png"
+  }
+]);
