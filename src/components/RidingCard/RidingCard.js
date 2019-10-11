ad3f660a6d6d3124510766434c2d804acd7ab33c
mr_ <njgriffiths@gmail.com>
2019-10-10 23:19:03 -0700

    added vote history to data. repositioned chrome & copy in web view

--- src/components/RidingCard/RidingCard.js
+++ src/components/RidingCard/RidingCard.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './RidingCard.css';
 
 import locator from './images/test-01.png';
 
 const RidingCard = (props) => {
+	let fullResults;
 	const data = props.data;
 
+	const voteHistory = data.eln_results.split(',');
 	const stat = data.variable_pct ? parseFloat(data.variable_pct).toFixed(1) + '%' : data.variable_total;
+
+	if (voteHistory[2] !== 'NA') {
+		fullResults = (
+			<Fragment>
+				<div className={`swatch ${voteHistory[2]}`}></div>
+				<p className="eln-year">’11</p>
+				<div className={`swatch ${voteHistory[1]}`}></div>
+				<p className="eln-year">’08</p>
+				<div className={`swatch ${voteHistory[0]}`}></div>
+				<p className="eln-year">’06</p>
+			</Fragment>
+		)
+	}
+
+	console.log(voteHistory)
 
 	return (
 		<li className="riding-card">
@@ -28,14 +45,10 @@ const RidingCard = (props) => {
 				
 				<div className="eln-results">	
 					<p className="subhead">Vote history</p>
-					<div className="swatch liberal"></div>
+					<div className={`swatch ${voteHistory[3]}`}></div>
 					<p className="eln-year">’15</p>
-					<div className="swatch conservative"></div>
-					<p className="eln-year">’11</p>
-					<div className="swatch conservative"></div>
-					<p className="eln-year">’08</p>
-					<div className="swatch ndp"></div>
-					<p className="eln-year">’06</p>
+					{ fullResults } 
+					
 				</div>
 		</li>
 	)
